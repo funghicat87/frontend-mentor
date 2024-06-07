597b48c7b634e99abf1a38636eccc628a92e4e03
funghicat87 <funghicat87@gmail.com>
2024-06-07 16:35:24 +0800

    完成NewsletterForm

--- src/pages/NewsletterForm.jsx
+++ src/pages/NewsletterForm.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import ImgTable from '../assets/NewsletterForm/illustration-sign-up-desktop.svg';
 import ImgMobile from '../assets/NewsletterForm/illustration-sign-up-mobile.svg';
+import ImgSuccess from '../assets/NewsletterForm/icon-success.svg';
+
 import { useForm } from 'react-hook-form';
+import { useState } from 'react';
 
 const NewsletterForm = () => { 
   const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+  const [ submitted, setSubmitted ] = useState(false); 
+  
+  const onSubmit = data => {
+    console.log(data);
+    setSubmitted(true);
+  };
+
   return (
-    <div className='w-full h-screen bg-[var(--CharcoalGrey)] flex items-center justify-center font-roboto text-[var(--DarkSlateGrey)]'>
+    <div className='w-full h-screen bg-[var(--CharcoalGrey)] flex md:items-center md:justify-center font-roboto text-[var(--DarkSlateGrey)]'>
       <style>
         {`
           :root {
@@ -18,20 +27,21 @@ const NewsletterForm = () => {
           
         `}
       </style>
-      <div className='lg:w-[850px] md:w-[700px] bg-white flex md:flex-row sm:flex-col-reverse rounded-3xl p-5 justify-between'>
-          <div className='lg:w-[400px] md:w-[330px] m-9 mr-14 my-auto'>
-            <h1 className='lg:text-5xl md:text-3xl font-bold'>
+      {!submitted ? (
+        <div className='lg:w-[850px] md:w-[700px] w-full bg-white flex flex-col-reverse md:flex-row md:rounded-3xl md:p-5 justify-end md:justify-between'>
+          <div className='flex flex-col lg:w-[400px] md:w-[330px] md:m-9 md:mr-14 md:my-auto p-6 md:p-0'>
+            <h1 className='text-5xl md:max-lg:text-3xl font-bold mt-5 md:mt-0'>
               Stay updated!
             </h1>
-            <p className='lg:mt-5 md:m-2'>
+            <p className='mt-5 md:max-lg:m-2'>
               Join 60,000+ product managers receiving monthly updates on:
             </p>
-            <ul className='list-none space-y-2 lg:mt-5 md:m-2'>
+            <ul className='list-none space-y-2 mt-5 md:max-lg:m-2'>
               <li className='pl-8 bg-[url(/src/assets/NewsletterForm/icon-list.svg)] bg-no-repeat'>Product discovery and building what matters</li>          
               <li className='pl-8 bg-[url(/src/assets/NewsletterForm/icon-list.svg)] bg-no-repeat'>Measuring to ensure updates are a success</li>
               <li className='pl-8 bg-[url(/src/assets/NewsletterForm/icon-list.svg)] bg-no-repeat'>And much more!</li> 
             </ul>
-            <form className='lg:mt-8 md:mt-5 flex flex-col' onSubmit={handleSubmit(onSubmit)}>
+            <form className='mt-8 md:max-lg:mt-5 flex flex-col' onSubmit={handleSubmit(onSubmit)}>
               <label htmlFor="Email" className='text-xs font-bold'>
               Email address
               </label>
@@ -50,14 +60,30 @@ const NewsletterForm = () => {
                 })}
                 />
                  {errors.Email && <span className='text-red-500 text-xs mt-1'>{errors.Email.message}</span>}
-              <input id="submit" name="submit" type="submit" value="Subscribe to monthly newsletter" className='lg:mt-4 md:mt-2 px-5 py-3 rounded-lg border-0 bg-[var(--DarkSlateGrey)] focus:outline-none mt-2 font-medium text-white hover:bg-gradient-to-r from-[#FF5075] to-[#FD6A35] hover:drop-shadow-[0_10px_10px_#FFC8C4]'/>
+              <input id="submit" name="submit" type="submit" value="Subscribe to monthly newsletter" className='mt-4 md:max-lg:mt-2 px-5 py-3 rounded-lg border-0 bg-[var(--DarkSlateGrey)] focus:outline-none mt-2 font-medium text-white hover:bg-gradient-to-r from-[#FF5075] to-[#FD6A35] hover:drop-shadow-[0_10px_10px_#FFC8C4]'/>
             </form>
-        </div>
+          </div>
         <div>
-          <img src={ImgTable} alt="ImgTable" className='hidden sm:block w-full' />
-          <img src={ImgMobile} alt="ImgMobile" className='block sm:hidden w-full' />
+          <img src={ImgTable} alt="ImgTable" className='hidden md:block w-full' />
+          <img src={ImgMobile} alt="ImgMobile" className='block md:hidden w-full' />
         </div>
-      </div>
+      </div>):(
+          <div className='flex flex-col w-full lg:w-[450px] md:w-[400px] md:m-9 md:mr-14 md:my-auto p-6 md:p-12 md:rounded-3xl bg-white justify-around'>
+            <div>
+              <img src={ImgSuccess} alt="ImgSuccess" className='w-16'/>
+              <h1 className='text-5xl md:max-lg:text-3xl font-bold mt-5'>
+                Thanks for subscribing!
+              </h1>
+              <p className='mt-5'>
+                A confirmation email has been sent to <span className='font-medium'>ash@loremcompany.com.</span> 
+                Please open it and click the button inside to confirm your subscription.
+              </p>
+            </div>
+            <button className='mt-9 px-5 py-3 rounded-lg border-0 bg-[var(--DarkSlateGrey)] focus:outline-none  font-medium text-white hover:bg-gradient-to-r from-[#FF5075] to-[#FD6A35] hover:drop-shadow-[0_10px_10px_#FFC8C4]'>
+              Dismiss message
+            </button>
+          </div>
+      )}
     </div>
   )
 }
